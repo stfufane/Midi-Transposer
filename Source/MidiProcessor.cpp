b23243872727ccce1dc24ce07ae1ad0798ea3c17
stfufane <albanese.stephane@gmail.com>
2020-04-28 02:05:51 +0200

    Simplify chord intervals mapping with const vector and modulo operations

--- Source/MidiProcessor.cpp
+++ Source/MidiProcessor.cpp
@@ -14,15 +14,11 @@ StringArray MidiProcessor::notes{ "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#
 StringArray MidiProcessor::chords{ "None", "maj", "min", "sus4", "maj7", "min7", "7", "m7b5" };
 
 MidiProcessor::MidiProcessor(AudioProcessorValueTreeState& stateToUse)
-    : mapping_notes(128, std::vector<int>(1, 0)),
+    : mappingNotes(12, std::vector<int>(1, 0)),
       noteParameters(notes.size()),
       chordParameters(notes.size()),
       state(stateToUse)
 {
-    for (int i = 0; i < 128; i++) {
-        mapping_notes[i][0] = i;
-    }
-
     state.state.addListener(this);
 
     inputChannelParameter = dynamic_cast<AudioParameterInt*>(state.getParameter(IDs::paramInChannel));
@@ -67,6 +63,7 @@ void MidiProcessor::process(MidiBuffer& midiMessages)
 void MidiProcessor::mapNote(int note, juce::uint8 velocity, bool noteOn, int time, MidiBuffer& processedMidi)
 {
     MidiMessage m;
+    int baseNote;
 
     // If the note changed, turn off the previous notes before adding the new ones.
     if (noteOn)
@@ -75,16 +72,17 @@ void MidiProcessor::mapNote(int note, juce::uint8 velocity, bool noteOn, int tim
         currentNotesOn.push_back(note);
         if (note != lastNoteOn && lastNoteOn > -1)
         {
-            for (int i = 0; i < mapping_notes[lastNoteOn].size(); i++) {
-                processedMidi.addEvent(MidiMessage::noteOff(outputChannel, mapping_notes[lastNoteOn][i], velocity), time);
+            baseNote = lastNoteOn % 12;
+            for (int i = 0; i < mappingNotes[baseNote].size(); i++) {
+                processedMidi.addEvent(MidiMessage::noteOff(outputChannel, lastNoteOn + mappingNotes[baseNote][i], velocity), time);
             }
         }
         lastNoteOn = note;
 
         // Loop on the corresponding mapping for the played note.
-        for (int i = 0; i < mapping_notes[note].size(); i++) {
-            if (mapping_notes[note][i] > 127) break;
-            processedMidi.addEvent(MidiMessage::noteOn(outputChannel, mapping_notes[note][i], velocity), time);
+        baseNote = note % 12;
+        for (int i = 0; i < mappingNotes[baseNote].size(); i++) {
+            processedMidi.addEvent(MidiMessage::noteOn(outputChannel, note + mappingNotes[baseNote][i], velocity), time);
         }
     }
     else 
@@ -103,18 +101,18 @@ void MidiProcessor::mapNote(int note, juce::uint8 velocity, bool noteOn, int tim
         // Otherwise it means the released note was not active so we don't need to do anything.
         if (note == lastNoteOn) 
         {
-            for (int i = 0; i < mapping_notes[note].size(); i++) {
-                if (mapping_notes[note][i] > 127) break;
-                processedMidi.addEvent(MidiMessage::noteOff(outputChannel, mapping_notes[note][i], velocity), time);
+            baseNote = note % 12;
+            for (int i = 0; i < mappingNotes[baseNote].size(); i++) {
+                processedMidi.addEvent(MidiMessage::noteOff(outputChannel, note + mappingNotes[baseNote][i], velocity), time);
             }
         }
 
         // If there were still some notes on, play the last one.
         if (note == lastNoteOn && currentNotesOn.size() > 0) {
             lastNoteOn = currentNotesOn.back();
-            for (int i = 0; i < mapping_notes[lastNoteOn].size(); i++) {
-                if (mapping_notes[lastNoteOn][i] > 127) break;
-                processedMidi.addEvent(MidiMessage::noteOn(outputChannel, mapping_notes[lastNoteOn][i], velocity), time);
+            baseNote = lastNoteOn % 12;
+            for (int i = 0; i < mappingNotes[baseNote].size(); i++) {
+                processedMidi.addEvent(MidiMessage::noteOn(outputChannel, lastNoteOn + mappingNotes[baseNote][i], velocity), time);
             }
         }
     }
@@ -127,67 +125,18 @@ void MidiProcessor::updateMapping()
 
     for (int i = 0; i < notes.size(); i++)
     {
-        addMappedNotes(i, noteParameters[i]->getIndex(), chordParameters[i]->getCurrentChoiceName());
+        addMappedNotes(i, noteParameters[i]->getIndex(), chordParameters[i]->getIndex());
     }
 }
 
-void MidiProcessor::addMappedNotes(const int from_note, const int to_note, const String chord)
+void MidiProcessor::addMappedNotes(const int from_note, const int to_note, const int chord)
 {
-    int note, mapped_note, new_size = 1;
-    if (chord == "maj" || chord == "min" || chord == "sus4") 
+    // Redimensionne le tableau de mapping
+    mappingNotes[from_note].resize(chordIntervals[chord].size());
+    for (int i = 0; i < mappingNotes[from_note].size(); i++)
     {
-        new_size = 3;
+        mappingNotes[from_note][i] = to_note - from_note + chordIntervals[chord][i];
     }
-    else if (chord == "Maj7" || chord == "m7" || chord == "7" || chord == "m7b5") 
-    {
-        new_size = 4;
-    }
-
-    for (int i = 0; i <= 10; i++) 
-    {
-        note = i * 12 + from_note;
-        mapped_note = i * 12 + to_note;
-        if (note < 128 && mapped_note < 128) 
-        {
-            // Redimensionne le tableau de mapping
-            mapping_notes[note].resize(new_size);
-
-            // Définit la note racine
-            mapping_notes[note][0] = mapped_note;
-
-            // Si aucun accord paramétré on s'arrête là.
-            if (chord == "None") 
-                continue;
-
-            // Définit si on est sur une tierce majeure, mineure ou une quarte
-            if (chord == "maj" || chord == "Maj7" || chord == "7") {
-                mapping_notes[note][1] = mapped_note + 4;
-            }
-            else if (chord == "m7" || chord == "min") {
-                mapping_notes[note][1] = mapped_note + 3;
-            }
-            else if (chord == "sus4") {
-                mapping_notes[note][1] = mapped_note + 5;
-            }
-
-            // La quinte dans tous les cas sauf m7b5
-            if (chord == "m7b5") {
-                mapping_notes[note][2] = mapped_note + 6;
-            }
-            else {
-                mapping_notes[note][2] = mapped_note + 7;
-            }
-
-            // 7eme majeure ou mineure.
-            if (chord == "Maj7") {
-                mapping_notes[note][3] = mapped_note + 11;
-            }
-            else if (chord == "m7" || chord == "7") {
-                mapping_notes[note][3] = mapped_note + 10;
-            }
-        }
-    }
-
 }
 
 void MidiProcessor::valueTreePropertyChanged(ValueTree& treeWhosePropertyChanged, const Identifier& property)
--- Source/MidiProcessor.h
+++ Source/MidiProcessor.h
@@ -33,7 +33,17 @@ public:
 
 private:
     AudioProcessorValueTreeState& state;
-    std::vector< std::vector<int> > mapping_notes;
+    const std::vector< std::vector<int> > chordIntervals = {
+        {0},            // None
+        {0, 4, 7},      // maj
+        {0, 3, 7},      // min
+        {0, 5, 7},      // sus4
+        {0, 4, 7, 11},  // maj7
+        {0, 3, 7, 10},  // min7
+        {0, 4, 7, 11},  // 7
+        {0, 3, 6, 10}   // m7b5
+    };
+    std::vector< std::vector<int> > mappingNotes;
 
     AudioParameterInt* inputChannelParameter;
     AudioParameterInt* outputChannelParameter;
@@ -47,5 +57,5 @@ private:
     
     void mapNote(int note, juce::uint8 velocity, bool noteOn, int time, MidiBuffer& processedMidi);
     void updateMapping();
-    void addMappedNotes(const int note_origine, const int new_note, const String chord);
+    void addMappedNotes(const int note_origine, const int new_note, const int chord);
 };
