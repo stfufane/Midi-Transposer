570f17e2b653ec668907a9fa57fcb3d31816788c
Stephane Albanese <albanese.stephane@gmail.com>
2024-03-11 23:45:20 +0100

    Fix bad use of FetchContent in the project cmake

--- Utils.cmake
+++ Utils.cmake
@@ -1,23 +1,14 @@
 # Macro to include a github project
 include(FetchContent QUIET)
-macro (update_from_git name repo branch populate)
-    message("Updating ${name} from git...")
+macro (retrieve_from_git name repo branch subdir)
+    message("Retrieving ${name} from git...")
     FetchContent_Declare(${name}
             GIT_REPOSITORY ${repo}
             GIT_SHALLOW TRUE
             GIT_PROGRESS TRUE
-            GIT_TAG ${branch})
-
-    # Somehow juce does not want to work with MakeAvailable :(
-    if (${populate})
-        FetchContent_GetProperties(${name})
-
-        if (NOT ${name}_POPULATED)
-            FetchContent_Populate(${name})
-        endif ()
-    else()
-        FetchContent_MakeAvailable(${name})
-    endif()
+            GIT_TAG ${branch}
+            SOURCE_SUBDIR ${subdir}
+    )
     message("${name} update finished")
 endmacro()
 
